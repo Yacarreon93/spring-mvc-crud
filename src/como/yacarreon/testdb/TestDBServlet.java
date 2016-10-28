152c2bfa6413780c1bf39702aca1453f64206fe6
Yasser Carreón <yacasia@gmail.com>
2016-10-28 15:54:03 -0500

    Tested database successful

--- src/como/yacarreon/testdb/TestDBServlet.java
+++ src/como/yacarreon/testdb/TestDBServlet.java
@@ -1,11 +1,14 @@
 package como.yacarreon.testdb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
 
 /**
  * Servlet implementation class TestDBServlet
@@ -18,8 +21,28 @@ public class TestDBServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		// set up connection variables 
+		String user="projects";
+		String pass="projects";		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/spring_demo?useSSL=false";
+		String driver = "com.mysql.jdbc.Driver";
+		
+		// get the connection to database
+		try {
+			
+			PrintWriter out = response.getWriter();
+			out.println("Connecting to database: " + jdbcUrl);
+			Class.forName(driver);			
+			Connection connection = DriverManager.getConnection(jdbcUrl, user, pass);
+			out.println("Success!");			
+			connection.close();			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+		
 	}
 
 }
