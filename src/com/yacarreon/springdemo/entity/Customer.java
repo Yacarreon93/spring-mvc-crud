fe64ffe5a79894144c77b5b93bcd5f0d8b0f1dfe
Yasser Carreón <yacasia@gmail.com>
2016-10-29 15:44:12 -0500

    Added Customer Entity

---
+++ src/com/yacarreon/springdemo/entity/Customer.java
@@ -0,0 +1,70 @@
+package com.yacarreon.springdemo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customer")
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lasstName;
+	
+	@Column(name="email")
+	private String email;
+	
+	public Customer() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLasstName() {
+		return lasstName;
+	}
+
+	public void setLasstName(String lasstName) {
+		this.lasstName = lasstName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lasstName=" + lasstName + ", email=" + email
+				+ "]";
+	}		
+	
+}
