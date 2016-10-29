8fc9c8a6fa8d48b4a5b8f2c2c90865b78e82324f
Yasser Carreón <yacasia@gmail.com>
2016-10-29 16:13:56 -0500

    Added Customer DAO

---
+++ src/com/yacarreon/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.yacarreon.springdemo.dao;
+
+import java.util.List;
+
+import com.yacarreon.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+	
+}
---
+++ src/com/yacarreon/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,37 @@
+package com.yacarreon.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.yacarreon.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	// need to inject the session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// create a query
+		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		
+		// execute query and get result
+		List<Customer> customers = theQuery.getResultList();
+		
+		return customers;
+	}
+
+}
