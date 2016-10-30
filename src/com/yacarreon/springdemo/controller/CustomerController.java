114cd33dedf35a067e242dfb8465b8e72005a66c
Yasser Carreón <yacasia@gmail.com>
2016-10-30 12:20:54 -0600

    Called customer service instead of DAO

--- src/com/yacarreon/springdemo/controller/CustomerController.java
+++ src/com/yacarreon/springdemo/controller/CustomerController.java
@@ -7,22 +7,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.yacarreon.springdemo.dao.CustomerDAO;
 import com.yacarreon.springdemo.entity.Customer;
+import com.yacarreon.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	// need to inject the customer dao
+	// need to inject the customer service
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 
 	@RequestMapping("/list")
 	public String listCustomers(Model model) {
 		
-		// get customer from the dao
-		List<Customer> customers = customerDAO.getCustomers();
+		// get customer from the service
+		List<Customer> customers = customerService.getCustomers();
 		
 		// add customers to model
 		model.addAttribute("customers", customers);
