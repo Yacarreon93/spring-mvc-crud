1b44a6f1691d1810bfc92c91c1c6790ab2c73151
Yasser Carreón <yacasia@gmail.com>
2016-10-30 12:33:59 -0600

    Sended new customer to model

--- src/com/yacarreon/springdemo/controller/CustomerController.java
+++ src/com/yacarreon/springdemo/controller/CustomerController.java
@@ -32,7 +32,13 @@ public class CustomerController {
 	}
 	
 	@GetMapping("/showFormForAdd")
-	public String showFormForAdd() {
+	public String showFormForAdd(Model model) {
+		
+		// create model attribute to bind form data
+		Customer customer = new Customer();
+		
+		model.addAttribute("customer", customer);
+		
 		return "customer-form";
 	}
 	
